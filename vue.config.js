a72bac7857a015764ad0afa2526e0b9bec181019
Wojciech Bębenek <wojciech.bebenek@outlook.com>
2022-06-20 15:26:26 +0200

    update

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   base: '/home',
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/top-up-project/'
+    : '/'
 })
